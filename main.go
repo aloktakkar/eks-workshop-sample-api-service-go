8ef753d29146bbad385e572908669d88150ef59e
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-08-27 12:01:43 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"log"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-  
-	log.Println("Listening...")
-	http.ListenAndServe(":8080", nil)
-  }
-  
- type response struct {
-	Message string   `json:"message"`
-	EnvVars []string `json:"env"`
-	Fib     []int    `json:"fib"`
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love this")
 }
 
-func fib() func() int {
-	a, b := 0, 1
-	return func() int {
-		a, b = b, a+b
-		return a
-	}
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
