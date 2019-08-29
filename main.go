1d4fe3dac1ca8270e6ee2c8ebdaf2bc7face7e1b
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-08-28 22:20:09 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"log"
+    "log"
+    "net/http"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-  
+
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
   }
