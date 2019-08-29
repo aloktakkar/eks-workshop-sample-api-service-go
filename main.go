a66bdabdbfd256b229570315f44b44eade020971
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-08-29 00:14:33 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
     "log"
-    "fmt"
     "net/http"
 )
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi")
-    })
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+	
+    log.Fatal(http.ListenAndServe(":8080", nil))
   }
