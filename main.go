b19d6aa7c9e42f7032d40cf94fcc83aead66634c
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-08-29 18:25:34 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,14 +9,11 @@ import (
 func main() {
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
+        http.Handle("/", http.FileServer(http.Dir("./environment/eks-workshop-sample-api-service-go/static")))
     })
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hi")
     })
-	log.Fatal(http.ListenAndServe(":8080", nil))
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-
     log.Fatal(http.ListenAndServe(":8080", nil))
   }
