536c30a74bd787ccdb8dd56288f579bd25579e21
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-09-02 23:45:12 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,14 +8,6 @@ import (
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintf(w, "This is an example of a NEW DOCKER...you are now load balancing!!!!!!!!!!!!!")
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi")
-    })
-    log.Fatal(http.ListenAndServe(":9080", nil))
+    log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))))
+    
   }
