d3740dfce85873802f7541c3471c2742564fd960
aloktakkar <53187472+aloktakkar@users.noreply.github.com>
2019-09-03 09:36:47 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)
     fmt.Fprintf(w, "This is an example of a NEW DOCKER...you are now load balancing!!!!!!!!!!!!!")
 
     log.Fatal(http.ListenAndServe(":9080", nil))
